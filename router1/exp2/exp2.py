a96b9b1eb1d4ed2bbf32aeac2661a348cf9a6243
osman-miskhodzhev <miskhodzhev.osman@yandex.ru>
2025-06-19 19:18:48 +0300

    работает, но рассинхрон, не знаю почему

--- router1/exp2/exp2.py
+++ router1/exp2/exp2.py
@@ -35,78 +35,78 @@ def change_channel(ch_num):
 def move_to_save():
     pyautogui.moveTo(save)
 
-test_dr = 1
+test_dr = 15
 
 current_ch = 1
 
 # Канал 1
 change_channel(1)
 for sub_ch in range(2, 6):
-    os.system(f'iperf3 -c 192.168.0.101 -t {test_dr} --logfile ex2-router1-channels-1-{sub_ch}.csv')
+    os.system(f'iperf3 -c 192.168.0.102 -t {test_dr} --logfile ex2-router1-channels-1-{sub_ch}.csv')
     print(sub_ch)
 
 # Канал 2
 change_channel(2)
 for sub_ch in range(3, 7):
-    os.system(f'iperf3 -c 192.168.0.101 -t {test_dr} --logfile ex2-router1-channels-2-{sub_ch}.csv')
+    os.system(f'iperf3 -c 192.168.0.102 -t {test_dr} --logfile ex2-router1-channels-2-{sub_ch}.csv')
     print(sub_ch)
 
 # Канал 3
 change_channel(3)
 for sub_ch in range(4, 8):
-    os.system(f'iperf3 -c 192.168.0.101 -t {test_dr} --logfile ex2-router1-channels-3-{sub_ch}.csv')
+    os.system(f'iperf3 -c 192.168.0.102 -t {test_dr} --logfile ex2-router1-channels-3-{sub_ch}.csv')
     print(sub_ch)
 
 # Канал 4
 change_channel(4)
 for sub_ch in range(5, 9):
-    os.system(f'iperf3 -c 192.168.0.101 -t {test_dr} --logfile ex2-router1-channels-4-{sub_ch}.csv')
+    os.system(f'iperf3 -c 192.168.0.102 -t {test_dr} --logfile ex2-router1-channels-4-{sub_ch}.csv')
     print(sub_ch)
 
 # Канал 5
 change_channel(5)
 for sub_ch in range(6, 10):
-    os.system(f'iperf3 -c 192.168.0.101 -t {test_dr} --logfile ex2-router1-channels-5-{sub_ch}.csv')
+    os.system(f'iperf3 -c 192.168.0.102 -t {test_dr} --logfile ex2-router1-channels-5-{sub_ch}.csv')
     print(sub_ch)
 
 # Канал 6
 change_channel(6)
 for sub_ch in range(7, 11):
-    os.system(f'iperf3 -c 192.168.0.101 -t {test_dr} --logfile ex2-router1-channels-6-{sub_ch}.csv')
+    os.system(f'iperf3 -c 192.168.0.102 -t {test_dr} --logfile ex2-router1-channels-6-{sub_ch}.csv')
     print(sub_ch)
 
 # Канал 7
 change_channel(7)
 for sub_ch in range(8, 12):
-    os.system(f'iperf3 -c 192.168.0.101 -t {test_dr} --logfile ex2-router1-channels-7-{sub_ch}.csv')
+    os.system(f'iperf3 -c 192.168.0.102 -t {test_dr} --logfile ex2-router1-channels-7-{sub_ch}.csv')
     print(sub_ch)
 
 # Канал 8
 change_channel(8)
 for sub_ch in range(9, 13):
-    os.system(f'iperf3 -c 192.168.0.101 -t {test_dr} --logfile ex2-router1-channels-8-{sub_ch}.csv')
+    os.system(f'iperf3 -c 192.168.0.102 -t {test_dr} --logfile ex2-router1-channels-8-{sub_ch}.csv')
     print(sub_ch)
 
 # Канал 9
 change_channel(9)
 for sub_ch in range(10, 14):
-    os.system(f'iperf3 -c 192.168.0.101 -t {test_dr} --logfile ex2-router1-channels-9-{sub_ch}.csv')
+    os.system(f'iperf3 -c 192.168.0.102 -t {test_dr} --logfile ex2-router1-channels-9-{sub_ch}.csv')
     print(sub_ch)
 
 # Канал 10
 change_channel(10)
 for sub_ch in range(11, 14):
-    os.system(f'iperf3 -c 192.168.0.101 -t {test_dr} --logfile ex2-router1-channels-10-{sub_ch}.csv')
+    os.system(f'iperf3 -c 192.168.0.102 -t {test_dr} --logfile ex2-router1-channels-10-{sub_ch}.csv')
     print(sub_ch)
 
 # Канал 11
 change_channel(11)
 for sub_ch in range(12, 14):
-    os.system(f'iperf3 -c 192.168.0.101 -t {test_dr} --logfile ex2-router1-channels-11-{sub_ch}.csv')
+    os.system(f'iperf3 -c 192.168.0.102 -t {test_dr} --logfile ex2-router1-channels-11-{sub_ch}.csv')
     print(sub_ch)
 
 # Канал 12
 change_channel(12)
 for sub_ch in range(13, 14):
-    os.system(f'iperf3 -c 192.168.0.101 -t {test_dr} --logfile ex2-router1-channels-12-{sub_ch}.csv')
+    os.system(f'iperf3 -c 192.168.0.102 -t {test_dr} --logfile ex2-router1-channels-12-{sub_ch}.csv')
     print(sub_ch)
